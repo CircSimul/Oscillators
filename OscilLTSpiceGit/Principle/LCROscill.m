68ac8c4ebf2d2750bca04a7e515a74159c378684
CircSimul <113874272+CircSimul@users.noreply.github.com>
2023-08-20 13:00:42 -0300

    Add files via upload

--- OscilLTSpiceGit/Principle/LCROscill.m
+++ OscilLTSpiceGit/Principle/LCROscill.m
@@ -21,30 +21,15 @@ timeOscil=linspace(0,5e-6,2000);
 
 for i=1:length(timeOscil)
 
-    VCoscilOut1(i)=exp(-ksi*wn*timeOscil(i))*(1/(wn*sqrt(1-ksi^2)))*a*sin(wn*sqrt(1-ksi^2)*timeOscil(i));
-
-    VCoscilOut2(i)=exp(-ksi*wn*timeOscil(i))*(1/(wn*sqrt(1-ksi^2)))*(-b)*ksi*wn*sin(wn*sqrt(1-ksi^2)*timeOscil(i));
-
-    VCoscilOut3(i)=exp(-ksi*wn*timeOscil(i))*b*cos(wn*sqrt(1-ksi^2)*timeOscil(i));
-
-    VCoscilOut(i)=VCoscilOut1(i)+VCoscilOut2(i)+VCoscilOut3(i);
-
-
-
-
-
-    VCoscilOutNew(i)=exp(-ksi*wn*timeOscil(i))*((1/(wn*sqrt(1-ksi^2)))*(a-b*ksi*wn)*sin(wn*sqrt(1-ksi^2)*timeOscil(i))+b*cos(wn*sqrt(1-ksi^2)*timeOscil(i)));
+    VCoscilOut(i)=exp(-ksi*wn*timeOscil(i))*((1/(wn*sqrt(1-ksi^2)))*(a-b*ksi*wn)*sin(wn*sqrt(1-ksi^2)*timeOscil(i))+b*cos(wn*sqrt(1-ksi^2)*timeOscil(i)));
 
 end
 	
 	
-
-
-
 	
 figure(1)	
 lWidth=1.5;
-plot(timeOscil,VCoscilOutNew,'r','LineWidth',lWidth)
+plot(timeOscil,VCoscilOut,'r','LineWidth',lWidth)
 xlabel("t(s)", "fontsize", 14)
 ylabel("Vc (V)", "fontsize", 14)
 set(gca, "linewidth", 2, "fontsize", 14)
